4d67693941ed554175d55fa2e1ef6730ec0e00d3
Jonathan Fielding <j.fielding@me.com>
2020-09-12 22:11:46 +0100

    feat: add node-fetch (to test pr)

--- dangerfile.ts
+++ dangerfile.ts
@@ -1,4 +1,9 @@
-import {message, danger} from "danger"
+import {warn, danger} from "danger"
 
-const newFiles = danger.git.created_files.join("- ")
-message("New Files in this PR: \n - " + newFiles);
+// Always update package-lock when you install a new module
+const packageChanged = danger.git.modified_files.includes('package.json');
+const lockfileChanged = danger.git.modified_files.includes('package-lock.json');
+
+if (packageChanged && !lockfileChanged) {
+  warn('Changes were made to package.json, but not to package-lock.json - <i>Perhaps you need to run `npm install`</i>');
+}
